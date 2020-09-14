5b333eb09f663be8fe1b856979936e3189edc850
Leroy Grant <lgrant146@gmail.com>
2020-09-14 14:25:41 -0500

    math part 1

---
+++ math.py
@@ -0,0 +1,28 @@
+num1 = 12 + 108
+print(num1)
+
+num2 = num1 - 10
+
+print(num2)
+
+num3 = num2 * 2
+
+print(num3)
+
+num4 = num3 / 44
+
+print(num4)
+
+num5 = num3 // 44
+
+print(num5)
+
+print(int(2.5))
+
+print(int(-2.5))
+
+print(float(5))
+
+print(5**2)
+
+print(5 * 2)
