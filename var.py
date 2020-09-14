cc28004f5aa8aaa762ac670296eb73596b64baad
Leroy Grant <lgrant146@gmail.com>
2020-09-14 14:43:29 -0500

    variable testing

---
+++ var.py
@@ -0,0 +1,24 @@
+from cmath import pi
+
+import keyword
+
+Attlee = 7
+Truman = 101
+Stalin = "a number"
+
+print(Attlee)
+print(Truman)
+print(Stalin)
+
+Stalin = 3.14
+
+print(Stalin)
+
+
+test = pi
+
+print(test)
+
+print(keyword.kwlist)
+
+
